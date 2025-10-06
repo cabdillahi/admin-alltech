067a6c6607916db3ad68a716afee7dfb1de27204
cabdillahi <cabdilahi020@gmail.com>
2025-10-06 13:34:55 +0300

    https

--- client/src/Page/Redex/api/api.ts
+++ client/src/Page/Redex/api/api.ts
@@ -1,4 +1,4 @@
 // export const Url = "http://localhost:5000/api";
-export const Url = "http://api.alltechcomp.com/api";
+export const Url = "https://api.alltechcomp.com/api";
 export const ErrorMessage = "server is down ";
 // done 
